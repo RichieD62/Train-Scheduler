8ad15d723867f1b145b45553eab88d8d98ca2cc0
Rich D <richie.donovan62@gmail.com>
2018-10-28 22:41:32 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAChzUIFhqUrcxkZ38Ao1-TwN4YTJQq8UY",
+    authDomain: "train-scheduler-project-450ee.firebaseapp.com",
+    databaseURL: "https://train-scheduler-project-450ee.firebaseio.com",
+    projectId: "train-scheduler-project-450ee",
+    storageBucket: "",
+    messagingSenderId: "856220011607"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
