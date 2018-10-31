bf8a47bbc77425baedda457562ad5f03812e8f2b
Rich D <richie.donovan62@gmail.com>
2018-10-30 22:52:12 -0500

    Need to figure out time keeper logic

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
-var config = {
-    apiKey: "AIzaSyAChzUIFhqUrcxkZ38Ao1-TwN4YTJQq8UY",
-    authDomain: "train-scheduler-project-450ee.firebaseapp.com",
-    databaseURL: "https://train-scheduler-project-450ee.firebaseio.com",
-    projectId: "train-scheduler-project-450ee",
-    storageBucket: "",
-    messagingSenderId: "856220011607"
-  };
-  firebase.initializeApp(config);
 
-  var database = firebase.database();
+
+
+
+var database = firebase.database();  
+var trainName = "";
+var destination = "";
+//var firstTime = moment("HH:mm");
+var firstTime = "";
+var frequency = "";
+
+$(document).ready(function(){
+
+    $("#submit-button").on("click", function(){
+        event.preventDefault();
+
+        trainName = $("#input-name").val().trim();
+        destination = $("#input-destination").val().trim();
+        firstTime = $("#input-firstTime").val().trim();
+        //firstTime = ($("#input-firstTime").val().trim(), "HH:mm").format("X");
+        frequency = $("#input-frequency").val().trim();
+
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTime: firstTime,
+            frequency: frequency
+        });
+    });
+
+    database.ref().on("child_added", function(snapshot){
+        var tmp = snapshot.val();
+        //var empStart = moment.unix(snapshot).format("HH:mm")
+        console.log(tmp);
+
+        //var nextArrival = firstTime + frequency;
+        //var minutesAway = nextArrival - currentTime;
+        //    
+        var tRow = $("<tr>"); 
+            var trainNameTd = $("<td>").html(tmp.trainName);
+            var destinationTd = $("<td>").html(tmp.destination);
+            var firstTimeTd = $("<td>").html(tmp.firstTime);
+            //var empStart = $("<td>").html(tmp.firstTime);
+            var frequencyTd = $("<td>").html(tmp.frequency);
+            var nextArrivalTd = $("<td>").html("");
+            var minutesAwayTd = $("<td>").html("");
+
+        tRow.append(trainNameTd, destinationTd, firstTimeTd, frequencyTd, nextArrivalTd, minutesAwayTd);
+        $("#myTable").append(tRow);
+        
+    })
+
+
+
+
+});
